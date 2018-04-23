6a5d57cb2c3293e8562ba87e070dbde9dc3b3d43
Viktor <viktor.pulyak@pixelplex.by>
2018-04-23 12:15:32 +0300

    Change config

--- truffle.js
+++ truffle.js
@@ -11,8 +11,8 @@ module.exports = {
 			host: "localhost",
 			address: "0xdefb0902a0c59657e0739a429166283a016ccda6",
 			port: 6082,
-			gas: 4800000,
-			gasPrice: 22000000000,
+			// gas: 4800000,
+			// gasPrice: 22000000000,
 			network_id: "*"
 		}
 	}
